f8920f33086696945716a724d5ba1472244eae60
Andrés Ullúa <andres.ullua@gmail.com>
2017-02-22 09:46:38 -0300

    Plugin standard file and basic structure

---
+++ ui-for-pods.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Plugin Name: UI for Pods Framework
+* Plugin URI: https://github.com/andresullua/ui-for-pods
+* Description: UI improvements for Pods Framework.
+* Version: 0.1beta
+* Author: Andrés Ullúa
+* Author URI: https://github.com/andresullua/
+* License: GPLv3
+*/
+
+
+add_action('admin_enqueue_scripts', 'load_ui_for_pods');
+
+function load_ui_for_pods() {
+  //css
+  wp_register_style('ui_css', plugin_dir_url(__FILE__).'css/style.css');
+  wp_enqueue_style('ui_css' );
+  //js
+  wp_register_script('ui_js', plugin_dir_url(__FILE__).'js/script.js');
+  wp_enqueue_script('ui_js');  
+}
