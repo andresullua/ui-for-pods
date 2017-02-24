dbe3069b33b8ea6151d207db22a1ca24996917e1
Andrés Ullúa <andres.ullua@gmail.com>
2017-02-24 11:53:00 -0300

    hidden_label

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-# ui-for-pods
-UI for Pods Framework
+# UI for Pods
 
 This is a work in progress to improve [Pods](http://pods.io/) user interface through additional CSS classes.
 
-The goal is to add custom titles, relate and group fields, show/hide fields with selects, etc.
+The goal is to add custom titles, placeholders, show/hide fields with selects, etc.
 
-## First Stage
+### First Stage
 - [x] Inline Title
 - [x] Custom Title
+- [x] Placeholder
+- [x] Hidden Label
 - [ ] Horizontal Rule
-- [ ] Related fields
 - [ ] Show/Hide Selects
 
-## Second Stage
+### Second Stage
 - [ ] Tabs
 - [ ] Custom user style
+
+---
+
+## How does it work?
+*Use the following text structures by inserting them in the Additional CSS classes field.*
+
+![Additional CSS classes](http://www.estudiocraneo.com/github/images/additional_css_classes.jpg)
+
+### Inline Title
+`pods_inline_title`
+
+
+### Custom Title
+`pods_title_{The Title}`
+
+#### Placeholder
+`pods_placeholder_{The Placeholder}`
+
+#### Hidden label
+`pods_hidden_label`
+
+---
+
+*Feel free to test the plugin and give your feedback!*
--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@ jQuery(function($){
 var UiForPods = function(){
   var _ = this;
   var form = '.pods-form';
+  var prefix  = 'pods_';
   
   var add_css = {
-    title: 'pods_title_'
+    title: prefix+'title_',
+    related: prefix+'related_',
+    placeholder: prefix+'placeholder_'
   }
   
   this.init = function(){
     titles();
+    placeholder();
     jQuery(form).show();
   }
 
@@ -22,15 +26,56 @@ var UiForPods = function(){
       var $tr = jQuery(this).closest('tr');
       var classes = $this.attr('class');
       var title = classes.slice(classes.indexOf(add_css.title)).replace(add_css.title,'').replace(/-/g, " ");
+      title = title.replace(title.slice(title.indexOf(prefix)),'');
       var id = 'title-'+e;
       if(title){
-        $this.removeClass(add_css.title+title);        
+        $this.removeClass(add_css.title+title);
         $tr.attr('id', id);
         jQuery('<h3 class="title" for="'+id+'">'+title+'</h3>').insertBefore($tr);
       }
     });
   }
-
+  
+  var related = function(){
+    //name: pods_related_{id}
+    jQuery("label[class*='"+add_css.related+"']").each(function(){
+      var $this = jQuery(this);
+      var $tr	= $this.closest('tr');
+      var classes = $this.attr('class');
+      var id = classes.slice(classes.indexOf(add_css.related)).replace(add_css.related,'');
+      var placeholder = '';
+      if(id){
+        debugger;
+        if(id.indexOf('_') > -1){
+          debugger;
+          placeholder = "Title";
+        }else if( id.indexOf('link_') > -1){
+          placeholder = "Link";
+          var n = id.replace('link_','');
+          if(jQuery('.'+buttonId+'text-'+n).length){
+            $this.hide();
+          }
+        }
+      }
+      $tr.find('input').attr('placeholder', placeholder);
+    })
+  }
+  
+  var placeholder = function(){
+    jQuery("label[class*='"+add_css.placeholder+"']").each(function(){
+      var $this = jQuery(this);
+      var $tr	= $this.closest('tr');
+      var classes = $this.attr('class');
+      var placeholder = classes.slice(classes.indexOf(add_css.placeholder)).replace(add_css.placeholder,'').replace(/-/g, " ");
+      placeholder = placeholder.replace(placeholder.slice(placeholder.indexOf(prefix)),'');
+      if(placeholder){
+        $this.removeClass(add_css.placeholder+placeholder);
+        $tr.find('input').attr('placeholder', placeholder);
+      }
+    })
+  }
+  
+  
   this.init();
 
 }
