f58c757c19bd5d2504a63a14ce982d2de7cc5f5a
Andrés Ullúa <andres.ullua@gmail.com>
2017-11-09 15:28:08 -0300

    Beautify

--- js/script.js
+++ js/script.js
@@ -1,88 +1,87 @@
 var uiForPods;
-jQuery(function($){
+jQuery(function($) {
   uiForPods = new UiForPods();
 });
 
-var UiForPods = function(){
+var UiForPods = function() {
   var _ = this;
   var form = '.pods-form';
-  var prefix  = 'pods_';
-  
+  var prefix = 'pods_';
+
   var add_css = {
-    title: prefix+'title_',
-    placeholder: prefix+'placeholder_',
-    hidden_input: prefix+'hidden_input',
-    horizontal_rule: prefix+'horizontal_rule'
-  }
-  
-  this.init = function(){
+    title: prefix + 'title_',
+    placeholder: prefix + 'placeholder_',
+    hidden_input: prefix + 'hidden_input'
+  };
+
+  this.init = function() {
     title();
     placeholder();
     hidden_input();
     horizontal_rule();
     jQuery(form).show();
-  }
-    
-  var title = function(){
-    jQuery("label[class*='"+add_css.title+"']").each(function(e){
+  };
+
+  var title = function() {
+    jQuery("label[class*='" + add_css.title + "']").each(function(e) {
       var $this = jQuery(this);
       var $tr = jQuery(this).closest('tr');
       var classes = $this.attr('class');
-      var title = classes.slice(classes.indexOf(add_css.title)).replace(add_css.title,'');
+      var title = classes.slice(classes.indexOf(add_css.title)).replace(add_css.title, '');
       title = getCleanText(title);
-  
-      var id = 'title-'+e;
-      if(title){
-        $this.removeClass(add_css.title+title);
+
+      var id = 'title-' + e;
+      if (title) {
+        $this.removeClass(add_css.title + title);
         $tr.attr('id', id);
-        jQuery('<h3 class="title" for="'+id+'">'+title+'</h3>').insertBefore($tr);
+        jQuery('<h3 class="title" for="' + id + '">' + title + '</h3>').insertBefore($tr);
       }
     });
-  }
-      
-  var placeholder = function(){
-    jQuery("label[class*='"+add_css.placeholder+"']").each(function(){
+  };
+
+  var placeholder = function() {
+    jQuery("label[class*='" + add_css.placeholder + "']").each(function() {
       var $this = jQuery(this);
-      var $tr	= $this.closest('tr');
+      var $tr = $this.closest('tr');
       var classes = $this.attr('class');
-      var placeholder = classes.slice(classes.indexOf(add_css.placeholder)).replace(add_css.placeholder,'');
+      var placeholder = classes.slice(classes.indexOf(add_css.placeholder)).replace(add_css.placeholder, '');
       placeholder = getCleanText(placeholder);
-      if(placeholder){
-        $this.removeClass(add_css.placeholder+placeholder);
+      if (placeholder) {
+        $this.removeClass(add_css.placeholder + placeholder);
         $tr.find('input').attr('placeholder', placeholder);
       }
-    })
-  }  
-  
-  var horizontal_rule = function(){
-    jQuery("label[class*='"+add_css.horizontal_rule+"']").each(function(){
+    });
+  };
+
+  var horizontal_rule = function() {
+    jQuery("label[class*='" + add_css.horizontal_rule + "']").each(function() {
       var $this = jQuery(this);
-      var $tr	= $this.closest('tr');
+      var $tr = $this.closest('tr');
       $this.removeClass(add_css.horizontal_rule);
-      jQuery('<tr class="'+add_css.horizontal_rule+'"><th></th><td></td></tr>').insertAfter($tr);
-    })
-  }
-  
-  var hidden_input = function(){
-    jQuery("label[class*='"+add_css.hidden_input+"']").each(function(){
+      jQuery('<tr class="' + add_css.horizontal_rule + '"><th></th><td></td></tr>').insertAfter($tr);
+    });
+  };
+
+  var hidden_input = function() {
+    jQuery("label[class*='" + add_css.hidden_input + "']").each(function() {
       var $this = jQuery(this);
-      var $tr	= $this.closest('tr');
+      var $tr = $this.closest('tr');
       $this.removeClass(add_css.hidden_input);
       $tr.find('td').addClass(add_css.hidden_input);
-    })
-  }
-  
+    });
+  };
+
   //helpers
-  function getCleanText(text){  
-    if(text.indexOf(prefix)>-1){
-      text = text.replace(text.slice(text.indexOf(prefix)),'');
-    }     
+  function getCleanText(text) {
+    if (text.indexOf(prefix) > -1) {
+      text = text.replace(text.slice(text.indexOf(prefix)), '');
+    }
     var start_pos = text.indexOf('{') + 1;
-    var end_pos = text.indexOf('}',start_pos);
-    var text_to_get = text.substring(start_pos,end_pos)
+    var end_pos = text.indexOf('}', start_pos);
+    var text_to_get = text.substring(start_pos, end_pos)
     return text_to_get;
-  }  
-  
+  };
+
   this.init();
 
 }
